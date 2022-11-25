4431b0f74137fc6714548a42b2aa2a848858fcad
TawanMenezes <tawanmenezes@gmail.com>
2022-11-25 10:49:09 -0300

    debounce na busca

--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 import { Photo } from 'src/app/Interfaces/Photo';
 
 @Component({
@@ -7,17 +8,27 @@ import { Photo } from 'src/app/Interfaces/Photo';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css'],
 })
-export class PhotoListComponent {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRouted: ActivatedRoute) {}
 
   ngOnInit() {
     this.photos = this.activatedRouted.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
   }
 
   onKey(event: any) {
-    this.filter = event.target.value;
+    this.debounce.next(event.target.value);
+    return this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
+  }
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
